c55794e0edfdb1759d1cd5ce7c0659159d91d7da
amani51036 <48814241+amani51036@users.noreply.github.com>
2019-05-27 23:16:53 +0530

    Add files via upload

---
+++ Plot2.R
@@ -0,0 +1,21 @@
+##PLOT 2
+
+#Reading Data
+power_data <- read.table("household_power_consumption.txt",skip=1,sep=";")
+
+#Adding Descriptive Names 
+names(power_data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+
+#Taking subset by spescifing date range
+ss_power<- subset(power_data,power_data$Date=="1/2/2007" | power_data$Date =="2/2/2007")
+
+
+ss_power$Date <- as.Date(ss_power$Date, format="%d/%m/%Y")
+ss_power$Time <- strptime(ss_power$Time, format="%H:%M:%S")
+ss_power[1:1440,"Time"] <- format(ss_power[1:1440,"Time"],"2007-02-01 %H:%M:%S")
+ss_power[1441:2880,"Time"] <- format(ss_power[1441:2880,"Time"],"2007-02-02 %H:%M:%S")
+
+
+# plot
+plot(ss_power$Time,as.numeric(as.character(ss_power$Global_active_power)),type="l",xlab="",ylab="Global Active Power (kilowatts)") 
+#
